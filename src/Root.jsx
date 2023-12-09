2a204d9987741d0bd746642a3eafb59bbf57385c
rezha4 <rezha.bahari@gmail.com>
2023-12-09 11:34:59 +0700

    finished fetching, but need to impelemnt sender, receiver so click logic on users

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,10 +4,26 @@ import { Navbar } from "./components/Navbar";
 import { Signup } from "./pages/Signup";
 import { useAuthContext } from "./hooks/useAuthContext";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Root() {
   const { user } = useAuthContext();
-  console.log(user)
+  const [messages, setMessages] = useState([]);
+
+  const fetchMessages = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/messages");
+      const data = await response.json();
+      console.log(data)
+      setMessages(data.messages);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
 
   const contactClass = "text-xl p-3 m-3 border-b-2";
   const messageClass = "p-2 m-2 max-w-lg w-fit bg-green-400 rounded";
@@ -17,29 +33,22 @@ function Root() {
       {user ? (
         <>
           <main className="flex">
-            <aside className="w-[25vw] h-[90vh] overflow-y-auto">
+            <aside className="w-[25vw] h-[90vh] overflow-y-auto border-r-2">
               <div>
                 <h1 className={contactClass}>Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
-                <h1 className="text-xl p-3 m-3">Username</h1>
               </div>
             </aside>
             <section className="w-[75vw] h-[90vh] flex flex-col">
               <div className="h-[90vh] overflow-y-auto">
-                <p className={messageClass}>hi</p>
+                {messages.map((message) => (
+                  <p key={message._id} className={messageClass}>
+                    {message.message}
+                  </p>
+                ))}
               </div>
-              <div className="w-[100%] h-[10vh]">
-                <form className="h-[100%] flex justify-center items-center gap-2">
-                  <button className="" type="submit">
+              <div className="w-[100%] h-[10vh] border-t-2">
+                <form className="h-[100%] flex justify-center items-center gap-0.5">
+                  <button className="border p-2 border-black" type="submit">
                     Send
                   </button>
                   <input
@@ -55,7 +64,9 @@ function Root() {
       ) : (
         <div className="text-center mt-8">
           <p>Please login to see content:</p>
-          <Link to={"/login"} className="text-2xl underline">Login</Link>
+          <Link to={"/login"} className="text-2xl underline">
+            Login
+          </Link>
         </div>
       )}
     </>
