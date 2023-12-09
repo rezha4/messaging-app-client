c150f4052ce711946c1cdaccc0cdf05ef550e47e
rezha4 <rezha.bahari@gmail.com>
2023-12-09 11:14:48 +0700

    fetching user from localstorage todo fetch users and messages on Root

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,89 +2,62 @@ import { Login } from "./pages/Login";
 import "./index.css";
 import { Navbar } from "./components/Navbar";
 import { Signup } from "./pages/Signup";
+import { useAuthContext } from "./hooks/useAuthContext";
+import { Link } from "react-router-dom";
 
 function Root() {
+  const { user } = useAuthContext();
+  console.log(user)
+
+  const contactClass = "text-xl p-3 m-3 border-b-2";
+  const messageClass = "p-2 m-2 max-w-lg w-fit bg-green-400 rounded";
   return (
     <>
       <Navbar />
-      <main className="flex">
-        <aside className="w-[25vw] h-[90vh] overflow-y-auto">
-          <div>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-            <h1 className="text-xl p-3 m-3">Username</h1>
-          </div>
-        </aside>
-        <section className="w-[75vw] h-[90vh] flex flex-col">
-          <div className="h-[90vh] overflow-y-auto">
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              veniam dolorem laborum, dolore expedita itaque pariatur autem,
-              similique beatae provident quia ipsa voluptatem placeat!
-              Repellendus dolore sapiente velit reprehenderit dicta!
-            </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">Lorem </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Consectetur quasi odio modi aperiam quaerat maiores. Aspernatur,{" "}
-            </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              veniam dolorem laborum, dolore expedita itaque pariatur autem,
-              similique beatae provident quia ipsa voluptatem placeat!
-              Repellendus dolore sapiente velit reprehenderit dicta!
-            </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">Lorem </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Consectetur quasi odio modi aperiam quaerat maiores. Aspernatur,{" "}
-            </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              veniam dolorem laborum, dolore expedita itaque pariatur autem,
-              similique beatae provident quia ipsa voluptatem placeat!
-              Repellendus dolore sapiente velit reprehenderit dicta!
-            </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">Lorem </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Consectetur quasi odio modi aperiam quaerat maiores. Aspernatur,{" "}
-            </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              veniam dolorem laborum, dolore expedita itaque pariatur autem,
-              similique beatae provident quia ipsa voluptatem placeat!
-              Repellendus dolore sapiente velit reprehenderit dicta!
-            </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">Lorem </p>
-            <p className="p-2 m-2 max-w-lg w-fit bg-green-500">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Consectetur quasi odio modi aperiam quaerat maiores. Aspernatur,{" "}
-            </p>
-          </div>
-          <div className="w-[100%] h-[10vh]">
-            <form className="h-[100%] flex justify-center items-center gap-2">
-              <button className="" type="submit">
-                Send
-              </button>
-              <input
-                type="text"
-                name="message"
-                className="border border-black w-[90%] h-[70%] rounded-md"
-              />
-            </form>
-          </div>
-        </section>
-      </main>
+      {user ? (
+        <>
+          <main className="flex">
+            <aside className="w-[25vw] h-[90vh] overflow-y-auto">
+              <div>
+                <h1 className={contactClass}>Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+                <h1 className="text-xl p-3 m-3">Username</h1>
+              </div>
+            </aside>
+            <section className="w-[75vw] h-[90vh] flex flex-col">
+              <div className="h-[90vh] overflow-y-auto">
+                <p className={messageClass}>hi</p>
+              </div>
+              <div className="w-[100%] h-[10vh]">
+                <form className="h-[100%] flex justify-center items-center gap-2">
+                  <button className="" type="submit">
+                    Send
+                  </button>
+                  <input
+                    type="text"
+                    name="message"
+                    className="border border-black w-[90%] h-[70%] rounded-md"
+                  />
+                </form>
+              </div>
+            </section>
+          </main>
+        </>
+      ) : (
+        <div className="text-center mt-8">
+          <p>Please login to see content:</p>
+          <Link to={"/login"} className="text-2xl underline">Login</Link>
+        </div>
+      )}
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
-    return (
-        <div className="h-[10vh] shadow-md flex justify-center items-center">
-            <h1 className="text-3xl font-bold">Messaging App</h1>
-        </div>
-    )
-}
+  return (
+    <div className="h-[10vh] shadow-md flex justify-center items-center">
+      <Link to={"/"}>
+        <h1 className="text-3xl font-bold">Messaging App</h1>
+      </Link>
+    </div>
+  );
+};
