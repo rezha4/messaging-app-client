edfdac0d3df6d754d420f4a23a4c7c1265007eeb
rezha4 <rezha.bahari@gmail.com>
2023-12-10 11:14:02 +0700

    implemented getmsg and postmsg bug on selecting user/receiver

--- src/Root.jsx
+++ src/Root.jsx
@@ -10,10 +10,13 @@ function Root() {
   const { user } = useAuthContext();
   const [messages, setMessages] = useState([]);
   const [users, setUsers] = useState([]);
+  const [receiver, setReceiver] = useState("");
+  const [message, setMessage] = useState("");
 
   const fetchMessages = async () => {
     try {
-      const response = await fetch("http://localhost:3000/messages");
+      const url = `http://localhost:3000/messages?sender=${user.user._id}&receiver=${receiver}`;
+      const response = await fetch(url);
       const data = await response.json();
       setMessages(data.messages);
     } catch (error) {
@@ -31,10 +34,28 @@ function Root() {
     }
   };
 
+  const postMessages = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/messages", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ sender: user.user._id, receiver, message }),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    fetchMessages();
     fetchUsers();
   }, []);
+
+  const handleUserClick = (id) => {
+    setReceiver(id);
+    fetchMessages();
+    console.log(user.user._id);
+    console.log(receiver);
+  };
 
   const contactClass = "text-xl p-3 m-3 border-b-2";
   const messageClass = "p-2 m-2 max-w-lg w-fit bg-green-400 rounded";
@@ -46,11 +67,15 @@ function Root() {
           <main className="flex">
             <aside className="w-[25vw] h-[90vh] overflow-y-auto border-r-2">
               <div>
-                {
-                  users.map(user => (
-                    <p key={user._id} className={contactClass}>{user.username}</p>
-                  ))
-                }
+                {users.map((user) => (
+                  <p
+                    key={user._id}
+                    className={contactClass}
+                    onClick={() => handleUserClick(user._id)}
+                  >
+                    {user.username}
+                  </p>
+                ))}
               </div>
             </aside>
             <section className="w-[75vw] h-[90vh] flex flex-col">
@@ -62,14 +87,21 @@ function Root() {
                 ))}
               </div>
               <div className="w-[100%] h-[10vh] border-t-2">
-                <form className="h-[100%] flex justify-center items-center gap-0.5">
-                  <button className="border p-2 border-black" type="submit">
+                <form
+                  onSubmit={postMessages}
+                  className="h-[100%] flex justify-center items-center gap-0.5"
+                >
+                  <button
+                    className="border p-2 border-black rounded"
+                    type="submit"
+                  >
                     Send
                   </button>
                   <input
                     type="text"
                     name="message"
                     className="border border-black w-[90%] h-[70%] rounded-md"
+                    onChange={(e) => setMessage(e.target.value)}
                   />
                 </form>
               </div>
